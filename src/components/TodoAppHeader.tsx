4a248890f9be5999c81e79ccd3761360508238fa
DefoortMerijn <merijn.defoort@student.howest.be>
2022-03-09 15:25:46 +0100

    feat: more layour changes

--- src/components/TodoAppHeader.tsx
+++ src/components/TodoAppHeader.tsx
@@ -1,7 +1,9 @@
+import '../style/components/todo-app-header.scss'
 const TodoAppHeader = () => {
   return (
-    <header>
-      <p>header</p>
+    <header className="c-todo-app-header">
+      <h1 className="c-todo-app-header__title">Hell {'Mort'}</h1>
+      <p className="c-todo-app-header__subtitle">There are {3} todos</p>
     </header>
   )
 }
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,10 +1,23 @@
-const TodoItem= () => {
-    return (
-      <header>
-        <p>item</p>
-      </header>
-    )
-  }
-  
-  export default TodoItem
-  
+import { Check } from 'lucide-react'
+import '../style/components/todo-item.scss'
+const TodoItem = () => {
+  return (
+    <div className="c-todo-item">
+      <input
+        className="c-todo-item-input"
+        type="checkbox"
+        name=""
+        id="check"
+      ></input>
+      <label className="c-todo-item-label" htmlFor="check">
+        <Check className="c-todo-item-symbol" />
+      </label>
+      <div className="c-todo-item-content">
+        <p className="c-todo-item-text">Planten water geven.</p>
+        <p className="c-todo-item-category">Work</p>
+      </div>
+    </div>
+  )
+}
+
+export default TodoItem
